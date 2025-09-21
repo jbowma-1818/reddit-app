b3c45bdd14e13b2f808b9d2f26c8993f8f2ea8a0
jbowma-1818 <justbow42@gmail.com>
2025-09-21 01:49:27 -0400

    Added spotify function  that allows connection to API

---
+++ Jammming/src/util/Spotify.jsx
@@ -0,0 +1,27 @@
+const clientId = 'fc30527be89341babea529d8aae5da19';
+const redirctUri = 'http://127.0.0.1:3000';
+let accessToken = '';
+
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken;
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+
+};
+
+export default Spotify;
