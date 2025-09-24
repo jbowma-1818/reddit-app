95677ac74d7683faaebcaace8a044048ea950a6b
jbowma-1818 <justbow42@gmail.com>
2025-09-24 19:09:14 -0400

    Implemented Tracklist component

--- Jammming/src/components/Tracklist/Tracklist.jsx
+++ Jammming/src/components/Tracklist/Tracklist.jsx
@@ -1,9 +1,14 @@
+import Track from '../Track/Track';
 
-function Tracklist(props) {
+function Tracklist({ tracks = [] }) {
+    if(!tracks.length){
+        return null;
+    }
 
     return (
-        <>
-        </>
+        <div className='tracklist'>
+            {tracks.map(track => <Track key={track.id} track={track} />)}
+        </div>
     );
 }
 
