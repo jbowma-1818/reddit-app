50b027e75fb02f3e3eb579729415da63b96cc986
jbowma-1818 <justbow42@gmail.com>
2025-09-24 19:42:01 -0400

    Implemented Track component

--- Jammming/src/components/Track/Track.jsx
+++ Jammming/src/components/Track/Track.jsx
@@ -1,25 +1,16 @@
-import { useState } from "react";
+function Track({ track }) {
+    if(!track){
+        return null;
+    }
 
-function Track() {
-    const [id, setId] = useState(0);
-
-    const incrementId = () => setId(id + 1);
-
-    let track = {
-        id: {incrementId},
-        name: 'unknown',
-        artist: 'unknown',
-        album: 'unknown',
-    };
+    const { name, artist, album } = track;
 
     return (
-        <>
-            <div>
-                <p>Name: {track}</p>
-                <p>Artist: {artist}</p>
-                <p>Album: {album}</p>
-            </div>
-        </>
+        <div>
+            <p>Name: {track}</p>
+            <p>Artist: {artist}</p>
+            <p>Album: {album}</p>
+        </div>
     );
 }
 
