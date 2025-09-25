a33bd5160b0c3c6c48b7db6228ba0532b68d9ae5
jbowma-1818 <justbow42@gmail.com>
2025-09-25 13:44:31 -0400

    Implemented ContactsPage component

--- appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
+++ appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContacts }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const [isDuplicate, setIsDuplicate] = useState(false);
+
+  // Checks to see if there is a duplicate
+  useEffect(() => {
+    setIsDuplicate(contacts.filter(({contact}) => contact.name === name));
+  }, [name, contacts]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +25,15 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if(isDuplicate){
+      return;
+    }
+
+    addContacts(name, phone, address);
+
+    setName("");
+    setPhone("");
+    setAddress("");
   };
 
   /*
@@ -26,10 +45,20 @@ export const ContactsPage = () => {
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          address={address}
+          setAddress={setAddress}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contactList={contacts} />
       </section>
     </div>
   );
