68e31cb7651c1aec1228f1fd410d3661b58c5915
jbowma-1818 <justbow42@gmail.com>
2025-09-25 15:33:41 -0400

    implemented TileList as a stateless component that renders a list of Tile components using an array of objects

--- appointment-planner-starting-v18/src/components/tileList/TileList.js
+++ appointment-planner-starting-v18/src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ list }) => {
   return (
     <div>
-      
+      {list.map(({name, ...rest}, index) => (
+        <Tile 
+          key={index}
+          name={name} 
+          description={rest}
+        />
+      ))}
     </div>
   );
 };
--- appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
+++ appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
@@ -58,7 +58,7 @@ export const ContactsPage = ({ contacts, addContacts }) => {
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList contactList={contacts} />
+        <TileList list={contacts} />
       </section>
     </div>
   );
