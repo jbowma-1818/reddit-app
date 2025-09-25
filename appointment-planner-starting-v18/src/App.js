243a8e5656652d97b193a1b0709677dd53254711
jbowma-1818 <justbow42@gmail.com>
2025-09-25 11:38:28 -0400

    Added state variables implemented two functions to add data to contacts and appointments

--- appointment-planner-starting-v18/src/App.js
+++ appointment-planner-starting-v18/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,11 +9,20 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const addContact = (name, phone, email) => {
+    setContacts((prevContacts) => [...prevContacts, {name, phone, email}]);
+  };
+
+  const addAppointments = (name, contact, date, time) => {
+    setAppointments((prevAppointments) => [...prevAppointments, {name, contact, date, time}]);
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
