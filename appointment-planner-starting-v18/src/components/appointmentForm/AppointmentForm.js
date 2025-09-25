031e48c6c8c513f8710cfa914d1196c12ab3d510
jbowma-1818 <justbow42@gmail.com>
2025-09-25 16:53:16 -0400

    Add appointment form functionality

--- appointment-planner-starting-v18/src/components/appointmentForm/AppointmentForm.js
+++ appointment-planner-starting-v18/src/components/appointmentForm/AppointmentForm.js
@@ -9,8 +9,10 @@ const getTodayString = () => {
 
 export const AppointmentForm = ({
   contacts,
-  title,
-  setTitle,
+  //title,
+  //setTitle,
+  name,
+  setName,
   contact,
   setContact,
   date,
@@ -21,6 +23,18 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+        <label htmlFor="contact">Contact</label>
+        <input id="contact" value={contact} onChange={(e) => setContact(e.target.value)}></input>        
+        <label htmlFor="date">Date</label>
+        <input id="date" type="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
+        <label htmlFor="time">Time</label>
+        <input id="time" type="time" value={time} onChange={(e) => setTime(e.target.value)}></input>
+        <button type="submit">Add Appointment</button>
+      </form>
+    </>
   );
 };
