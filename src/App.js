c888931cb631e2175108d8c09aae15f1ad9bcd1f
jbowma-1818 <justbow42@gmail.com>
2025-10-14 23:51:01 -0400

    Incoporated Post component into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
+      <Post />
     </div>
   );
 }
