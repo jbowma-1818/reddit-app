96993023b796b5641ad5e5c6ab49e3c54001d856
jbowma-1818 <justbow42@gmail.com>
2025-10-15 17:26:57 -0400

    Added JS expression inside the JSX code

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,17 +4,17 @@ export default function Post({ likeCount = 0, title = 'Untitled', author = 'Unkn
     return (
         <div className='post'>
             <div className='post-container'>
-                <span className='post-like-count'>15</span>
+                <span className='post-like-count'>{likeCount}</span>
                 <div className='post-info-container'>
-                    <h2 className='post-title'>Test Title</h2>
+                    <h2 className='post-title'>{title}</h2>
                     <div className='image-container'>
                         <img alt='' className='post-image' />
                     </div>
                 </div>
                 <div className='post-footer-container'>
-                    <span className='post-author'>Author</span>
-                    <span className='post-timeframe'>1 hour ago</span>
-                    <span className='post-comments-count'>5</span>
+                    <span className='post-author'>{author}</span>
+                    <span className='post-timeframe'>{timeframe}</span>
+                    <span className='post-comments-count'>{commentCount}</span>
                 </div>
             </div>    
         </div>
