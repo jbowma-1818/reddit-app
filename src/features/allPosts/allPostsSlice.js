e922f14385200afcc018b4f1a9219673b6b6461c
jbowma-1818 <justbow42@gmail.com>
2025-10-17 18:48:32 -0400

    Updated allPostSlice to include different loading states

--- src/features/allPosts/allPostsSlice.js
+++ src/features/allPosts/allPostsSlice.js
@@ -3,17 +3,36 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const loadPosts = createAsyncThunk(
     'allPosts/getAllPosts',
     async (postId, thunkAPI) => {
-        const data = await fetch(postId);
-        return Response.data;
+        const data = await fetch(postId); // Need to change what's inside fetch
+        return data.json();
     }
 );
 
 export const allPostsSlice = createSlice({
     name: 'allPosts',
-    initialState: [],
-    reducers: {
+    initialState: {
+        posts: [],
+        isLoading: false, 
+        hasError: false
+    },
+    reducers: { // might be able to have this be blank
         loadData: (state, action) => {
-            state.push(action.payload);
+            state.posts.push(action.payload);
+        }
+    },
+    extraReducers: {
+        [loadPosts.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadPosts.fulfilled]: (state, action) => {
+            state.posts.push(action.payload);
+            state.isLoading = false;
+            state.hasError = true;
+        },
+        [loadPosts.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
         }
     }
 });
