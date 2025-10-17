d91a64d0bddad3f9514694371b820e4077f23734
jbowma-1818 <justbow42@gmail.com>
2025-10-17 18:06:38 -0400

    Created slice for allPostsSlice

---
+++ src/features/allPosts/allPostsSlice.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadPosts = createAsyncThunk(
+    'allPosts/getAllPosts',
+    async (postId, thunkAPI) => {
+        const data = await fetch(postId);
+        return Response.data;
+    }
+);
+
+const allPostsSlice = createSlice({
+    name: 'allPosts',
+    initialState: [],
+    reducers: {
+        loadData: (state, action) => {
+            state.push(action.payload);
+        }
+    }
+});
