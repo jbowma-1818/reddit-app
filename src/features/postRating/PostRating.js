9904899a72c3e78ac03eb3c1d61d8c014f398ff3
jbowma-1818 <justbow42@gmail.com>
2025-10-20 18:26:05 -0400

    Updated how numbers are displayed

--- src/features/postRating/PostRating.js
+++ src/features/postRating/PostRating.js
@@ -4,6 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 const PostRating = ({ likeCount }) => {
     const dispatch = useDispatch();
 
+    // Handles if likeCount is greater than 999
+    if(likeCount >= 1000 && likeCount < 100000000){
+        likeCount = (likeCount/1000).toPrecision(3) + 'k';
+    }else if(likeCount >= 100000000){
+        likeCount = (likeCount/100000000).toPrecision(3) + 'mil';
+    }
+
     return (
         <>
             <button className='arrow'>&#8679;</button>
