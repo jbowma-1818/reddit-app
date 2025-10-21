db52e5949123cee23c8bac9bd1fba58a4ba49690
jbowma-1818 <justbow42@gmail.com>
2025-10-20 21:01:29 -0400

    Decided to change PostRating.js to be part of the Components folder because each post will have its own individual ratings

--- src/components/PostRating.js
+++ src/components/PostRating.js
@@ -3,18 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const PostRating = ({ likeCount }) => {
     const dispatch = useDispatch();
+    let likeCountString = '';
 
     // Handles if likeCount is greater than 999
     if(likeCount >= 1000 && likeCount < 100000000){
-        likeCount = (likeCount/1000).toPrecision(3) + 'k';
+        likeCountString = (likeCount/1000).toFixed(1) + 'k';
     }else if(likeCount >= 100000000){
-        likeCount = (likeCount/100000000).toPrecision(3) + 'mil';
+        likeCountString = (likeCount/100000000).toFixed(1) + 'mil';
+    }else{
+        likeCountString = toString(likeCount);
     }
 
     return (
         <>
             <button className='arrow'>&#8679;</button>
-            <p className='like-rating'>{likeCount}</p>
+            <p className='like-rating'>{likeCountString}</p>
             <button className='arrow' style={{transform: 'rotate(180deg)'}}>&#8679;</button>
         </>
     );
