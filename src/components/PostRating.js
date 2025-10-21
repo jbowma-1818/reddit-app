a7e0186b8d72efcfa0990cfa78e1b72d36af4fb4
jbowma-1818 <justbow42@gmail.com>
2025-10-20 21:18:55 -0400

    Added functions to handle voting

--- src/components/PostRating.js
+++ src/components/PostRating.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const PostRating = ({ likeCount }) => {
     const [rating, setRating] = useState(likeCount);
+    const [voted, setVoted] = useState(0); // Helps to handle how the user has voted 
 
     // Formats count to be converted to a string string
     const formatLikes = (count) => {
@@ -9,6 +10,32 @@ const PostRating = ({ likeCount }) => {
         if(count >= 1_000) return (count / 1_000).toFixed(1) + 'k';
         return count.toString();
     };
+
+    // Handles upvote
+    const handleUpVote = () => {
+        // Handles if the user had already selected the upvote button and instead it undoes the vote
+        if(voted === 1){ 
+            setRating(rating - 1);
+            setVoted(0);
+        } else {
+            // Handles if downvote was selected or if no button was selected
+            setRating(voted === -1 ? rating + 2 : rating + 1);
+            setVoted(1);
+        }
+    }
+
+    // Handles downvote
+    const handleDownVote = () => {
+        // Handles if the user had already selected the downvote button and instead it undoes the vote
+        if(voted === -1){ 
+            setRating(rating + 1);
+            setVoted(0);
+        } else {
+            // Handles if upvote was selected or if no button was selected
+            setRating(voted === 1 ? rating - 2 : rating - 1);
+            setVoted(-1);
+        }
+    }
 
     return (
         <>
