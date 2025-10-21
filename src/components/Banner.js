ea52348cf39fd1cf612dd574322e53e42dc41070
jbowma-1818 <justbow42@gmail.com>
2025-10-21 02:22:07 -0400

    Updated the position of the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,10 +6,12 @@ export default function Banner() {
     return (
         <div className='banner'>
             <div className='banner-container'>
+                <div className='banner-title-logo-container'>
                 <img src={redditLogo} alt='Reddit Logo' className='banner-icon'></img>
-                <p className='banner-title'><span className='text-color'>Reddit</span>Minimal</p> 
+                <p className='banner-title'><span className='text-color'>Reddit</span>Minimal</p>                     
+                </div>
+                <SearchBar />
             </div>
-            <SearchBar />
         </div>
     );
 }
