ebe847b0f7d027631b0d6d2fc4db777f9180a2bb
jbowma-1818 <justbow42@gmail.com>
2025-10-21 20:16:37 -0400

    Set up search bar to be a form

--- src/features/searchBar/SearchBar.js
+++ src/features/searchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchBarTerm, clearSearchBarTerm, selectSearchBarTerm } from './searchBarSlice';
+import { loadPosts } from '../allPosts/allPostsSlice';
 
 const SearchBar = () => {
     const dispatch = useDispatch();
@@ -16,8 +17,21 @@ const SearchBar = () => {
         dispatch(clearSearchBarTerm());
     };
 
+    // Handle search submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Will go to popular page if search is empty
+
+        if(searchTerm.trim() === '') {
+            dispatch(loadPosts());
+        } else {
+            const searchUrl = `https://www.reddit.com/search.json?q=${encodeURIComponent(searchTerm)}`;
+            dispatch(loadPosts(searchUrl));
+        }            
+    }
+
     return (
-        <div className='search-bar'>
+        <form className='search-bar' onSubmit={handleSubmit}>
             <div className='search-bar-container'>
                 <div className='search-bar-input'>
                     <input 
@@ -29,7 +43,7 @@ const SearchBar = () => {
                 </div>
                 <button onClick={handleClear}><span>&#x24E7;</span></button>
             </div>
-        </div>
+        </form>
     );
 };
 
