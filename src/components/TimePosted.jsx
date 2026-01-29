d5c3c99b528f99a86c49d40c1944007bb53987ae
jbowma-1818 <justbow42@gmail.com>
2026-01-29 16:25:42 -0500

    Fixed issue where dateObj needed to be converted to string and have currTimeMilliseconds be accurate

--- src/components/TimePosted.jsx
+++ src/components/TimePosted.jsx
@@ -2,12 +2,12 @@
 const TimePosted = ({ timePosted }) => {
     function timeConversion(timePosted) {
         const timePostedInMilliseconds = timePosted * 1000;
-        const currTimeMilliseconds = Date.now() * 1000;
+        const currTimeMilliseconds = Date.now();
         const dateObj = new Date(currTimeMilliseconds - timePostedInMilliseconds);
 
-        if(dateObj.getHours() < 1) return `${dateObj.getMinutes()} min ago`;
-        if(dateObj.getHours() >= 24) return `${dateObj.getDay()} days ago`;
-        return `${dateObj.getHours()} hrs ago`;
+        if(dateObj.getHours() < 1) return `${dateObj.getMinutes().toLocaleString()} min ago`;
+        if(dateObj.getHours() >= 24) return `${dateObj.getDay().toLocaleString()} days ago`;
+        return `${dateObj.getHours().toLocaleString()} hrs ago`;
     };
 
     return <div className='post-time-posted'>{timeConversion(timePosted)}</div>
