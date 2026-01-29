10735e8f047deadf0d87dd3483e332c3c88e81b3
jbowma-1818 <justbow42@gmail.com>
2026-01-29 16:19:46 -0500

    Improved the ability for post to be able to say how long ago they were posted

--- src/components/TimePosted.jsx
+++ src/components/TimePosted.jsx
@@ -2,9 +2,12 @@
 const TimePosted = ({ timePosted }) => {
     function timeConversion(timePosted) {
         const timePostedInMilliseconds = timePosted * 1000;
-        const dateObj = new Date(timePostedInMilliseconds).toLocaleTimeString();
+        const currTimeMilliseconds = Date.now() * 1000;
+        const dateObj = new Date(currTimeMilliseconds - timePostedInMilliseconds);
 
-        return dateObj;
+        if(dateObj.getHours() < 1) return `${dateObj.getMinutes()} min ago`;
+        if(dateObj.getHours() >= 24) return `${dateObj.getDay()} days ago`;
+        return `${dateObj.getHours()} hrs ago`;
     };
 
     return <div className='post-time-posted'>{timeConversion(timePosted)}</div>
